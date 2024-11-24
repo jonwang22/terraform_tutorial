54bf051b8f1b827d10cd12678e2805c5c58e6033
Jon Wang <155007142+jonwang22@users.noreply.github.com>
2024-11-24 00:41:40 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+# Make sure you have a .tfvars file that can hold any sensitive values like access keys or passwords.
+# Anything that you do not want revealed in a repository or publically, put it into the .tfvars file
+# The .tfvars file within this repo is an example template for reference. 
+# Normally, .tfvars is in the gitignore and should never be pushed to a repo
+
 variable access_key{
   type=string
   sensitive=true
